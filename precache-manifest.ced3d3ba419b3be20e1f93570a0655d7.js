5dcbbdd23a9b97d5f3b8a01bd857e5c42e47c810
chriserl <chriserldev@gmail.com>
2020-08-21 07:06:41 +0000

    Updates

---
+++ precache-manifest.ced3d3ba419b3be20e1f93570a0655d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a202f1f562ea2d89096807261f7fd48",
+    "url": "/retube/index.html"
+  },
+  {
+    "revision": "688a4fde67f1ca86852e",
+    "url": "/retube/static/css/main.70fb5f4e.chunk.css"
+  },
+  {
+    "revision": "332d492d4a93d231f9f3",
+    "url": "/retube/static/js/2.6daa9151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/retube/static/js/2.6daa9151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688a4fde67f1ca86852e",
+    "url": "/retube/static/js/main.37fd311e.chunk.js"
+  },
+  {
+    "revision": "18d775ffb98442ee514f",
+    "url": "/retube/static/js/runtime-main.f2e54f4c.js"
+  }
+]);
