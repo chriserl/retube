3bf6f8ebe2fb96105669caba5484c49b0d6d89e9
chriserl <chriserldev@gmail.com>
2020-08-21 07:11:18 +0000

    setup base typography styling

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
   return (
     <div className="App">
       <p className="h1">App</p>
-      <p className="h2">App</p>
-      <p className="h3">App</p>
-      <p className="h4">App</p>
-      <p className="pl">App</p>
-      <p className="plb">App</p>
-      <p className="ps">App</p>
-      <p className="psb">App</p>
     </div>
   );
 }
