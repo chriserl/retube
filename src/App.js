c6fee9c554961e978736b1b1a657022701ffe530
chriserl <chriserldev@gmail.com>
2020-08-21 07:05:41 +0000

    created base font styling with test for mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./sass/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <p>App</p>
+      <p className="h1">App</p>
+      <p className="h2">App</p>
+      <p className="h3">App</p>
+      <p className="h4">App</p>
+      <p className="pl">App</p>
+      <p className="plb">App</p>
+      <p className="ps">App</p>
+      <p className="psb">App</p>
     </div>
   );
 }
